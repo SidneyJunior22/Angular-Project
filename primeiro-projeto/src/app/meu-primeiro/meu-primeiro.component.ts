ad1fce729a26d64c460ed456177721c98f1e6b8f
Sidney Júnior <smjunior_22@hotmail.com>
2020-06-11 20:10:47 +0100

    First Commit

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,11 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'meu-primeiro-component',
+  template: `
+  <p>
+  Meu primeiro componente com Angular! </p>
+  `
+})
+export class MeuPrimeiroComponent { }
